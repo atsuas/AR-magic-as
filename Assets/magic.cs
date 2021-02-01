c809d63a21f83563094cf85a26eeec65c13b5c4e
as <>
2021-02-02 05:32:09 +0900

    効果音の実装

--- Assets/magic.cs
+++ Assets/magic.cs
@@ -6,11 +6,14 @@ public class Magic : MonoBehaviour
 {
     public ParticleSystem particle;
     public ParticleSystem particle1;
+    public AudioClip audioClip;
+    public AudioClip audioClip1;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = transform.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,10 +27,14 @@ public class Magic : MonoBehaviour
         if (transform.tag == "Power")
         {
             particle.Play();
+            audioSource.clip = audioClip;
+            audioSource.Play();
         }
         else if (transform.tag == "Bomb")
         {
             particle1.Play();
+            audioSource.clip = audioClip1;
+            audioSource.Play();
         }
     }
 }
